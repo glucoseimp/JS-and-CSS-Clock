fc223cfc02a21736733bc5952abc19fd0b086c9b
glucoseimp <kseniiakozukhova@gmail.com>
2017-05-25 16:04:53 +0300

    addad a second hand`s movement

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const secondHand = document.querySelector(".hand--second");
+
+
+
+function setDate() {
+	const now = new Date();
+
+	const seconds = now.getSeconds();
+	const secondsDegrees = ((seconds / 60) * 360) + 90;
+
+	secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+	console.log(seconds);
+}
+
+setInterval(setDate, 1000)
